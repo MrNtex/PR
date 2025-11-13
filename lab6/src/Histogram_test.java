02842e1fcf314340dc0944acdb262df94ac7f55b
Artur <56607215+MrNtex@users.noreply.github.com>
2025-11-13 17:54:24 +0100

    Add files via upload

---
+++ lab6/src/Histogram_test.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+
+class Histogram_test {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Set image size: n (#rows), m(#kolumns)");
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        Obraz obraz_1 = new Obraz(n, m);
+
+        obraz_1.calculate_histogram();
+        obraz_1.print_histogram();
+
+        // Wariant - 1
+        int num_threads = 94;
+
+        WatekHistogramu[] NewThr = new WatekHistogramu[num_threads];
+
+        for (int i = 0; i < num_threads; i++) {
+            (NewThr[i] = new WatekHistogramu(obraz_1, obraz_1.getSymbol(i), i)).start();
+        }
+
+        for (int i = 0; i < num_threads; i++) {
+            try {
+         	    NewThr[i].join();
+            } catch (InterruptedException _) {}
+        }
+
+        obraz_1.compareHistograms();
+
+        obraz_1.clean_histogram_parallel();
+
+        // Wariant - 2
+        System.out.println("Set number of threads");
+        num_threads = scanner.nextInt();
+
+        Thread[] threads = new Thread[num_threads];
+
+        int symbolsPerThread = 94 / num_threads;
+        int remainder = 94 % num_threads;
+
+        int start = 0;
+        for (int i = 0; i < num_threads; i++) {
+            int extra = (i < remainder) ? 1 : 0;
+            int end = start + symbolsPerThread + extra;
+
+            threads[i] = new Thread(new WatekHistogramu2(obraz_1, start, end));
+            threads[i].start();
+
+            start = end;
+        }
+
+        for (int i = 0; i < num_threads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException _) {}
+        }
+
+        obraz_1.compareHistograms();
+    }
+
+}
+
