6d68459bf6c8d9024906df3b5fcccaf3cc55ffe7
Artur <56607215+MrNtex@users.noreply.github.com>
2025-11-13 19:09:23 +0100

    5.0

--- lab6/src/Histogram_test.java
+++ lab6/src/Histogram_test.java
@@ -84,6 +84,67 @@ class Histogram_test {
 
         obraz_1.sum_local_histograms();
         obraz_1.compareHistograms();
+
+        // 5.0 - Dekompozycja 2D
+        obraz_1.clean_histogram_parallel();
+
+        System.out.println("Set number of threads");
+        num_threads = scanner.nextInt();
+
+        threads = new Thread[num_threads];
+        obraz_1.create_local_histogram(num_threads);
+
+        int totalSize = obraz_1.getSize_n() * obraz_1.getSize_m();
+        int blockSize = totalSize / num_threads;
+
+        for (int i = 0; i < num_threads; i++) {
+            int b_start = i * blockSize;
+            int b_end = (i == num_threads - 1) ? totalSize : (i + 1) * blockSize;
+
+            threads[i] = new Thread(new WatekHistogramuDek2d(i, b_start, b_end, obraz_1));
+            threads[i].start();
+        }
+
+        for (int i = 0; i < num_threads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException _) {}
+        }
+
+        obraz_1.sum_local_histograms();
+        obraz_1.compareHistograms();
+
+        // 5.0 - Dekompozycja 3D
+        obraz_1.clean_histogram_parallel();
+
+        System.out.println("Set number of threads");
+        num_threads = scanner.nextInt();
+
+        threads = new Thread[num_threads*2];
+        obraz_1.create_local_histogram(num_threads*2);
+
+        totalSize = obraz_1.getSize_n() * obraz_1.getSize_m();
+        blockSize = totalSize / num_threads;
+
+        for (int i = 0; i < num_threads*2; i+=2) {
+            int b_start = i/2 * blockSize;
+            int b_end = (i/2 == num_threads - 1) ? totalSize : (i/2 + 1) * blockSize;
+
+            threads[i] = new Thread(new WatekHistogramuDek3d(i, b_start, b_end, 0, 94/2, obraz_1));
+            threads[i].start();
+
+            threads[i+1] = new Thread(new WatekHistogramuDek3d(i+1, b_start, b_end, 94/2, 94, obraz_1));
+            threads[i+1].start();
+        }
+
+        for (int i = 0; i < num_threads*2; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException _) {}
+        }
+
+        obraz_1.sum_local_histograms();
+        obraz_1.compareHistograms();
     }
 
 }
